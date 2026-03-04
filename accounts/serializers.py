e3130c300e3d4ef4593db19ba916c098cd916a60
Okedi Ismail Musa <okediismail820@gmail.com>
2026-03-04 18:36:59 +0300

    created login serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
@@ -37,3 +38,25 @@ class RegisterSerializer(serializers.ModelSerializer):
             phone_number=validated_data.get('phone_number','')
         )
         return user
+    
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username-username, password=password)
+
+            if not user:
+                raise serializers.ValidationError("Invalid username or password")
+            if not user.is_active:
+                raise serializers.ValidationError("user account is disabled")
+            else:
+                raise serializers.ValidationError("Must include username and password")
+            
+            data['user'] = user
+            return data
