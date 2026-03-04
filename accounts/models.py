a6492479ec15eb7a2460e28a79f31dcace5775ee
Okedi Ismail Musa <okediismail820@gmail.com>
2026-03-04 07:40:09 +0300

    my first model

---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('STUDENT', 'Student'),
+        ('SUPERVISOR', 'Supervisor'),
+        ('ADMIN', 'Administrator'),
+    ]
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='STUDENT'
+        )
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
