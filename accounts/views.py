45e2bc381a1a100f0b817329455ffb3770056905
Okedi Ismail Musa <okediismail820@gmail.com>
2026-03-04 09:35:50 +0300

    created view and url plus testing api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+
+@api_view(['GET'])
+def user_list(request):
+
+    users = User.objects.all()
+
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
+
+
 
 # Create your views here.
