3a04f1fa617d97889d3254c692fe6ead817e05e5
Okedi Ismail Musa <okediismail820@gmail.com>
2026-03-04 19:06:36 +0300

    updated urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('users/', views.user_list, name='user-list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('me/', views.me, name='me'),
 ]
